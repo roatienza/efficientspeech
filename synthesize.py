c4bab59c8158ace6e8b13b49fdd7b710f641085f
roatienza <rowel@eee.upd.edu.ph>
2022-06-03 19:58:42 +0800

    onnx

--- synthesize.py
+++ synthesize.py
@@ -118,7 +118,7 @@ def load_module(args, pl_module, preprocess_config, lexicon=None, g2p=None):
         print("Converting to ONNX ...", args.onnx)
         #pl_module.to_onnx
         pl_module.eval()
-        torch.onnx.export(pl_module, (x,), args.onnx, export_params=True,
+        torch.onnx.export(pl_module, x, args.onnx, export_params=True,
                           opset_version=11, do_constant_folding=True,
                           input_names=["phoneme"], output_names=["wav"],
                           dynamic_axes={
